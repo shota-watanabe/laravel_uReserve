d7f67d2bd93b124c345c550d16ea1fdbdb2259e4
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-10-10 13:11:21 +0900

    livewire test

---
+++ app/Http/Livewire/Counter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 0;
+
+    public $name = '';
+
+    public function mount()
+    {
+      $this->name = 'mount';
+    }
+
+    // リアルタイムバリデーションなどに使う
+    public function updated()
+    {
+      $this->name = 'updated';
+    }
+
+    public function mouseOver()
+    {
+      $this->name = 'mouseOver';
+    }
+
+    public function increment()
+    {
+      $this->count++;
+    }
+
+    public function render()
+    {
+      return view('livewire.counter');
+    }
+}
